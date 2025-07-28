040fd4480ba6af48f3eb0b81a9b1ec7847f0f337
seshu <chaitumulagani@gmail.com>
2025-07-28 15:22:37 +0530

    env and node_modules are added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const http = require("http")
-const port = 2000;
+const http = require("http");
+require('dotenv').config()
 const server =  http.createServer((req, res)=> {
     console.log("Hello From Server....");
-    res.write("Hello From Server...");
+    res.write("Hello From Server..............");
     res.end();
 });
 
+const port = process.env.PORT
 server.listen(port, ()=> {
     console.log(`http://localhost:${port}`);
 })
